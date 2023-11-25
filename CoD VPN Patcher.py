af27e153743a62ecc6b6d0e42a1561c6bfbbe505
Anton Murer <antonmurer.03@gmail.com>
2023-11-25 15:44:15 +0100

    removed ovpn profile

--- CoD VPN Patcher.py
+++ CoD VPN Patcher.py
@@ -2,35 +2,60 @@ import sys
 from tkinter import messagebox
 
 NAME = "CoD VPN Patcher"
+
 def error(msg, file=""):
     messagebox.showerror(NAME, msg + "\n" + file)
     exit(1)
 
+def patch_ovpn(path: str):
+    f = open(path, "r")
+    splitted_file = f.read().split("<ca>")
+
+    if (len(splitted_file) <= 1):
+        error("This (.ovpn) file can not be patched by " + NAME, path)
+
+    if ("route-nopull\nroute 185.34.0.0 255.255.0.0" in splitted_file[0]):
+        error("(.ovpn) File is already patched", path)
+
+    splitted_file[0] += "route-nopull\nroute 185.34.0.0 255.255.0.0\n<ca>" # Adding modification to VPN file
+    patched_vpn_file = ""
+    for splitter in splitted_file: # Reassemnling content of VPN File
+        patched_vpn_file += splitter
+
+    f.close()
+    new_vpn_file = path.split(".ovpn")[0] + "_CoD_VPN.ovpn"
+    f = open(new_vpn_file, "w")
+    f.write(patched_vpn_file)
+    f.close()
+    messagebox.showinfo(NAME, "Successfully patched!\nNew VPN profile at:\n" + new_vpn_file)
+    exit(0)
+
+def patch_wireguard(path: str):
+    f = open(path, "r")
+    vpn_text = f.read()
+    if ("AllowedIPs = 185.34.0.0/16" in vpn_text):
+        error("(.conf) File is already patched", path)
+
+    if ("AllowedIPs = 0.0.0.0/0" in vpn_text):
+        vpn_text = vpn_text.replace("0.0.0.0/0", "185.34.0.0/16")
+        f.close()
+        new_vpn_file = path.split(".conf")[0] + "_CoD_VPN.conf"
+        f = open(new_vpn_file, "w")
+        f.write(vpn_text)
+        f.close()
+        messagebox.showinfo(NAME, "Successfully patched!\nNew VPN profile at:\n" + new_vpn_file)
+        exit(0)
+    else:
+        error(f"This (.conf) file can not be patched by " + NAME, path)
+
+
 if (len(sys.argv) == 1 or len(sys.argv) >= 3):
-    error("Drag an (.ovpn) file over the (.bat) file to patch it")
+    error("Drag an (.ovpn/.conf) file over the (.bat) file to patch it")
 
-if (sys.argv[1].split('.')[-1] != "ovpn"):
-    error("Not the correct file type. (.ovpn) file needed", sys.argv[1])
+if (sys.argv[1].split('.')[-1] == "ovpn"):
+    patch_ovpn(sys.argv[1])
 
-vpn_file = sys.argv[1]
-f = open(vpn_file, "r")
-splitted_file = f.read().split("<ca>")
+if (sys.argv[1].split('.')[-1] == "conf"):
+    patch_wireguard(sys.argv[1])
 
-if (len(splitted_file) <= 1):
-    error("This (.ovpn) file can not be patched by " + NAME, vpn_file)
-
-if ("route-nopull\nroute 185.34.0.0 255.255.0.0" in splitted_file[0]):
-    error("(.ovpn) File is already patched", vpn_file)
-
-splitted_file[0] += "route-nopull\nroute 185.34.0.0 255.255.0.0\n<ca>" # Adding modification to VPN file
-patched_vpn_file = ""
-for splitter in splitted_file: # Reassemnling content of VPN File
-    patched_vpn_file += splitter
-
-f.close()
-new_vpn_file = vpn_file.split(".ovpn")[0] + "_CoD_VPN.ovpn"
-f = open(new_vpn_file, "w")
-f.write(patched_vpn_file)
-f.close()
-messagebox.showinfo(NAME, "Successfully patched!\nNew VPN profile at:\n" + new_vpn_file)
-exit(0)
+error("Not the correct file type. (.ovpn/.conf) file needed", sys.argv[1])
--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # CoD-VPN-Patcher
-Patches OpenVPN Profiles (.ovpn) to CoD VPN profiles.
+Patches OpenVPN/WireGuard Profiles (.ovpn/.conf) to CoD VPN profiles.
 
 # Warining
-This is something I've put together in a few minutes! It probably works with most/all OpenVPN profiles but that is not guaranteed. Use it at your own risk!
+This is something I've put together in a few minutes! It probably works with most/all OpenVPN/WireGuard profiles but that is not guaranteed. Use it at your own risk!
 
 ## Requirements
-1. A VPN provider that offers .ovpn files to download (e.g. NordVPN)
-2. The newest version of [OpenVPN](https://openvpn.net/community-downloads/)
+1. A VPN provider that offers .ovpn or .conf files to download (e.g. NordVPN)
+2. The newest version of [OpenVPN](https://openvpn.net/community-downloads/) or [WireGuard](https://www.wireguard.com/install/)
 3. [Python](https://www.python.org/downloads/) 3.9 or higher, check your Python version by opening a Terminal and typing "python --version" (only tested on python 3.11.6, should work tho)
 
 ## Installation
 1. Download this repository as .zip
 2. Extract the folder and drag it somewhere it belongs (e.g. your programs folder)
 3. (optional) For easier access you can create a shortcut for the .bat file and put it on your Desktop
-4. Download .ovpn file of your VPN provider
-5. Drag .ovpn over the "CoD VPN Patcher.bat" file or the shortcut
-6. A new file will be created with the name "\<original file name>_CoD_VPN.opvn"
-7. Import the new VPN file to OpenVPN
+4. Download .ovpn/.conf file of your VPN provider
+5. Drag the VPN file over the "CoD VPN Patcher.bat" file or the shortcut
+6. A new file will be created with the name "\<original file name>_CoD_VPN.\<ovpn/conf>"
+7. Import the new VPN file into OpenVPN/WireGuard
 8. Profit?
